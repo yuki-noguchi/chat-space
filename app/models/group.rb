cf3bbd0db1701b10972e789a10ca5fee15b49556
yuki-noguchi <y.n.10191@gmail.com>
2017-04-29 18:39:42 +0900

    Add association

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,4 @@
 class Group < ApplicationRecord
+  has_many :users, through: :group_users
+  accepts_nested_attributes_for :group_users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :name, presence: true
+  has_many :groups, through: :group_users
+  accepts_nested_attributes_for :group_users
 end
