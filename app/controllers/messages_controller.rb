0d3c0caebf9eefefb503dd46d76893d88b81e0ad
yuki-noguchi <y.n.10191@gmail.com>
2017-04-29 15:31:32 +0900

    Apply flash messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,4 @@
 class MessagesController < ApplicationController
   def index
-    @flash = {}
-    @flash[:sign_in] = "ログインに成功しました"
-    @flash[:sign_in_error] = "ログインに失敗しました"
-    @flash[:sign_up] = "ユーザー情報を登録しました"
-    @flash[:sign_up_error] = "ユーザー情報の登録に失敗しました"
-    @flash[:sign_out] = "ログアウトしました"
-    @flash[:edit] = "ユーザー情報を編集しました"
   end
 end
