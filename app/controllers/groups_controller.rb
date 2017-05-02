1a727ccc02ea91fc38e3724ea705a8e35465ce8b
yuki-noguchi <y.n.10191@gmail.com>
2017-05-02 20:50:31 +0900

    Add conditional branch

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,8 +9,12 @@ class GroupsController < ApplicationController
   end
 
   def create
-    Group.create(group_params)
-    redirect_to root_path
+    group = Group.new(group_params)
+    if group.save
+      redirect_to root_path
+    else
+      redirect_to new_group_path
+    end
   end
 
   private
