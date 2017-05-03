ccfa854cdf8b8d584d0648fb89efb1b91598acd5
yuki-noguchi <y.n.10191@gmail.com>
2017-05-03 12:29:46 +0900

    Delete unnecessary description

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,16 +5,13 @@ class GroupsController < ApplicationController
 
   def new
     @group = Group.new
-    @users = User.all
   end
 
   def create
-    group = Group.new(group_params)
-    if group.save
+    @group = Group.new(group_params)
+    if @group.save
       redirect_to root_path, notice: 'グループ作成に成功しました'
     else
-      @group = Group.new
-      @users = User.all
       flash.now[:error] = 'グループの作成に失敗しました'
       render :new
     end
