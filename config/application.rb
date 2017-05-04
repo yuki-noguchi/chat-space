b2ee35ae982d3c56c59a2be9c0a2b8efe6e80d1b
yuki-noguchi <y.n.10191@gmail.com>
2017-05-04 18:58:50 +0900

    Change time notation

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 module ChatSpace
   class Application < Rails::Application
     config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     config.generators do |g|
       g.javascripts false
       g.helper false
