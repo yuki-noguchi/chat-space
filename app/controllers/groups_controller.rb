4a2fbb7832942b21d4899f2c217b865ac7dac408
yuki-noguchi <y.n.10191@gmail.com>
2017-05-04 17:15:39 +0900

    Delete show action

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,44 @@
+class GroupsController < ApplicationController
+
+  before_action :set_group, only: [:edit, :update]
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループ作成に成功しました'
+    else
+      flash.now[:error] = 'グループの作成に失敗しました'
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @group.update(group_params)
+      redirect_to group_path, notice: 'グループ編集に成功しました'
+    else
+      flash.now[:error] = 'グループの編集に失敗しました'
+      render :edit
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, { user_ids: [] })
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
+  end
+
+end
