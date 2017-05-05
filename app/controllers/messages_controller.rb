b0f64bcefaf748dfcdeb0b2120485214692e53a4
yuki-noguchi <y.n.10191@gmail.com>
2017-05-05 16:47:59 +0900

    Add flash messages when user message sending

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,18 +1,27 @@
 class MessagesController < ApplicationController
+
+  before_action :set_group, only: [:index, :create]
   def index
-    @group = Group.find(params[:group_id])
     @groups = current_user.groups.order('created_at DESC')
     @message = Message.new
   end
 
   def create
-    Message.create(message_params)
-    redirect_to action: :index
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to group_messages_path(@group), notice: 'メッセージの送信に成功しました'
+    else
+      redirect_to group_messages_path(@group), alert: 'メッセージの送信に失敗しました'
+    end
   end
 
   private
 
   def message_params
     params.require(:message).permit(:body).merge(user_id: current_user.id, group_id: params[:group_id])
+  end
+
+  def set_group
+    @group = Group.find(params[:group_id])
   end
 end
