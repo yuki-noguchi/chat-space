19581b7a92ba80e58fce232064a40a232babf815
yuki-noguchi <y.n.10191@gmail.com>
2017-05-06 20:25:53 +0900

    Re-set before action

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -3,14 +3,17 @@ require 'rails_helper'
 describe MessagesController, type: :controller do
   let(:user) { create(:user) }
 
-  describe 'GET #index'do
-
     before do
       login_user user
     end
 
+  describe 'GET #index'do
+
+    before do
+      get :index, params: { group_id: 28 }
+    end
+
     it "renders the :index template" do
-      get :index, {group_id: 28}
       expect(response).to render_template :index
     end
 
