b2c2e3f49950ff9ed41b7d5558fceaf8c77669a4
yuki-noguchi <y.n.10191@gmail.com>
2017-05-06 19:55:35 +0900

    Test if index action renders template

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
-describe MessagesController do
+describe MessagesController, type: :controller do
+  let(:user) { create(:user) }
+
+  describe 'GET #index'do
+
+    before do
+      login_user user
+    end
+
+    it "renders the :index template" do
+      get :index, {group_id: 28}
+      expect(response).to render_template :index
+    end
+
+  end
 
 end
