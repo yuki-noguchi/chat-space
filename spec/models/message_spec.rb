a7fad1d1a7d7b24b7e48f82197bb1d2243b22a28
yuki-noguchi <y.n.10191@gmail.com>
2017-05-06 14:35:18 +0900

    Add test if message can be saved

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,11 +1,20 @@
 require 'rails_helper'
 
 describe Message do
+
   describe '#create' do
+
     it "is invalid without a body" do
       message = build(:message, body: nil)
       message.valid?
       expect(message.errors[:body]).to include("を入力してください")
     end
+
+    it "is valid with a body" do
+      message = build(:message)
+      expect(message).to be_valid
+    end
+
   end
+
 end
