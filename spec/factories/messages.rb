b712863660f2d667fdba52a441ca37374a45faa2
yuki-noguchi <y.n.10191@gmail.com>
2017-05-09 21:56:13 +0900

    Set factories, faker and association

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
 
   factory :message do
     body Faker::Lorem.sentence
-    group_id 28
-    user_id 1
+    user
+    group
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,5 +6,11 @@ FactoryGirl.define do
     email                 Faker::Internet.email
     password              pass
     password_confirmation pass
+
+    after(:create) do |user|
+      temp_group = create(:group)
+      create(:message, user: user, group: temp_group)
+      create(:group_user, user: user, group: temp_group)
+    end
   end
 end
