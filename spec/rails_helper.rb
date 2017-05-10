9bb8ee7e1f90efb53d2bae660f949ed306b5171b
yuki-noguchi <y.n.10191@gmail.com>
2017-05-10 13:07:56 +0900

    Rewrite shortly

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,7 +6,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'devise'
-require File.expand_path("spec/support/controller_macros.rb")
+require 'support/controller_macros.rb'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
