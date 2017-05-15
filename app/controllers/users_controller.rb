f9ccf8dbd334ef955cbdd87f84935c5c3af91683
yuki-noguchi <y.n.10191@gmail.com>
2017-05-15 17:21:44 +0900

    Add search action that is to search user name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+  def search
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+    respond_to do |format|
+      format.json { render 'groups/new', json: @users }
+    end
+  end
 end
