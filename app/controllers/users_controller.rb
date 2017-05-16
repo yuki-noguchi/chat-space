4f972214f965335ca72018231486ccc4d09f143f
yuki-noguchi <y.n.10191@gmail.com>
2017-05-16 15:37:55 +0900

    Exclude current user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def search
-    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where.not(name: current_user.name)
     respond_to do |format|
       format.json { render 'groups/new', json: @users }
     end
