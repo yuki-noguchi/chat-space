f6f4f8c3fa62534df8e5a2698caaba0781600efc
yuki-noguchi <y.n.10191@gmail.com>
2017-05-19 12:55:46 +0900

    Add config for testing

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,7 +3,11 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   include CarrierWave::RMagick
 
-    storage  :fog
+  if Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
 
     def store_dir
         "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,20 +3,15 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
- config.storage = :fog
- config.fog_credentials = {
-   provider: 'AWS',
-   aws_access_key_id: ENV['ACCESS_KEY_ID'],
-   aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
-   region: 'ap-northeast-1'
- }
+  config.storage = :fog
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV['ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
+    region: 'ap-northeast-1'
+  }
 
-   case Rails.env
-   when 'development'
-       config.fog_directory  = ENV['BUCKET']
-       config.asset_host = ENV['AWS_S3_URL']
-   when 'production'
-       config.fog_directory  = ENV['BUCKET']
-       config.asset_host = ENV['AWS_S3_URL']
-   end
+  config.fog_directory  = ENV['BUCKET']
+  config.asset_host = ENV['AWS_S3_URL']
+
 end
