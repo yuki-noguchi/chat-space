dc4e4c6a0398dda570ed1b3d9da92215521476bc
yuki-noguchi <y.n.10191@gmail.com>
2017-05-19 13:05:59 +0900

    Delete unnecessary indent

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -10,7 +10,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
     def store_dir
-        "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+      "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
     end
 
 
